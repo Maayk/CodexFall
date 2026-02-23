2714f8713ba060dda9cb269918202b5d2b767253
Hope-rp <maykgamerzz@gmail.com>
2026-02-23 03:57:03 -0300

    add dynamic breadcrumb navigation to item details
    - Implementation of dynamic breadcrumb navigation in the header.
    
    - Categorization logic based on the hierarchy of items (Database > Category > Subcategory > Item).

--- assets/house-YSREXzgD.js
+++ assets/house-YSREXzgD.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B5OsapEl.js";/**
+import{c as a}from"./index-CybVADW9.js";/**
  * @license lucide-vue-next v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
